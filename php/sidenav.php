8951bba44e7d24bf41dca77e1ceef6014cefcfa6
unknown <abgmhafizhan1908@gmail.com>
2025-02-12 12:56:49 +0800

    Change sidenav font color to black

--- php/sidenav.php
+++ php/sidenav.php
@@ -40,7 +40,7 @@ if ($profilePicturePath == "") {
 
     <style>
         .sidebar {
-            background-color: #38761D;
+            background-color: #12b65d;
             color: white;
             position: fixed; /* Fixed positioning */
             top: 0;
@@ -69,22 +69,22 @@ if ($profilePicturePath == "") {
 
 <body>
     <div class="col-auto col-md-3 col-xl-2 px-0 sidebar">
-        <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+        <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-black min-vh-100">
             <img src="<?php echo $profilePictureSrc; ?>" class="rounded-circle mx-auto mt-3 bg-white" style="width: 150px; height: 150px; object-fit: cover;">
             <span class="fs-5 fw-bold mt-2 text-center w-100"><?php echo $_SESSION['fullname'] ?></span>
             <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start w-100 mt-3" id="menu">
                 <li class="nav-item w-100">
-                    <a href="redirect.php" class="text-white nav-link align-middle px-0">
+                    <a href="redirect.php" class="text-black nav-link align-middle px-0">
                         <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
                     </a>
                 </li>
                 <li class="nav-item w-100">
-                    <a href="settings.php" class="text-white nav-link align-middle px-0">
+                    <a href="settings.php" class="text-black nav-link align-middle px-0">
                         <i class="fs-4 bi-gear"></i> <span class="ms-1 d-none d-sm-inline">Settings</span>
                     </a>
                 </li>
                 <li class="nav-item w-100">
-                    <a href="logout.php" class="text-white nav-link align-middle px-0">
+                    <a href="logout.php" class="text-black nav-link align-middle px-0">
                         <i class="fs-4 bi-box-arrow-right"></i> <span class="ms-1 d-none d-sm-inline">Logout</span>
                     </a>
                 </li>
