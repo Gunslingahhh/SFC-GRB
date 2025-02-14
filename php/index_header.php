dfb07e6d1b61c44ed8de479eb62f5437f94b41ef
unknown <abgmhafizhan1908@gmail.com>
2025-02-14 16:48:36 +0800

    Adjust logo position

--- php/index_header.php
+++ php/index_header.php
@@ -17,10 +17,10 @@ include "connection.php";
 <body>
     <nav class="navbar navbar-expand-md bg-body-tertiary p-0">
         <div class="container-fluid">    
-            <a class="navbar-brand d-flex align-items-center" href="#">
+            <a class="ps-5 ms-5 navbar-brand d-flex align-items-center" href="#">
                 <img src="assets/image/logo.png" alt="SFC-GRB System" height="100">
             </a>
-            <div class="ms-2 d-flex flex-column header-text fw-semibold">
+            <div class="pe-5 me-5 d-flex flex-column header-text fw-semibold">
                 <h3 class="header-text fw-semibold text-center">Sarawak Forestry Corporation Genetic Resource Bank (SFC-GRB)</h3>
             </div>
             <div class="d-flex">
